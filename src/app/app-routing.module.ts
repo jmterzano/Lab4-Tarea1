e9d527459c974c0076b8b16c8168af48d3efd765
Juan <jmterzano@gmail.com>
2020-09-10 02:32:19 -0300

    primercomit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalculadoraComponent } from './componentes/calculadora/calculadora.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:CalculadoraComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+}) 
 export class AppRoutingModule { }
---
+++ src/app/componentes/calculadora/calculadora.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent implements OnInit {
+  numero1=0;
+  numero2=0;
+  promedio=0;
+  suma=0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  calcularPromedio(){
+    this.promedio=(this.numero1+this.numero2)/2;
+    this.suma=this.numero1+this.numero2;
+  }
+  limpiarCuadro(){
+    this.promedio=0;
+    this.suma=0;
+  }
+
+}
